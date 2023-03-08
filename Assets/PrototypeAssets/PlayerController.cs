13dcec35fb525b66f7ce38063d52f0c00c4bf522
James Hartley <jdbhartley@gmail.com>
2023-03-07 16:38:21 -0800

    Basic movement + Cinemachine Camera

--- Assets/PrototypeAssets/PlayerController.cs
+++ Assets/PrototypeAssets/PlayerController.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector3 velocity;
+    CharacterController _characterController;
+    [SerializeField] float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Apply velocity to character controller.
+        _characterController.Move(velocity * moveSpeed * Time.deltaTime);
+    }
+
+    //Called from PlayerInput
+    void OnMove(InputValue value) {
+        //Convert input values to usable velocity;
+        velocity = value.Get<Vector2>().normalized;
+        velocity.x *= -1;
+        velocity.z = -velocity.y;
+        velocity.y = 0;
     }
 }
